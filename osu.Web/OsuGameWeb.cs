9c1ae8022fdfafbf264845bc59d8178cd0972eee
kone <70107937+konekowo@users.noreply.github.com>
2023-06-27 11:21:33 -0400

    web part 1 :skull:

---
+++ osu.Web/OsuGameWeb.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game;
+
+namespace osu.Web
+{
+    internal partial class OsuGameWeb : OsuGame
+    {
+
+        public OsuGameWeb(string[]? args = null)
+            : base(args)
+        {
+        }
+
+    }
+}
---
+++ osu.Web/Program.cs
@@ -0,0 +1,48 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using osu.Framework;
+using osu.Framework.Platform;
+using osu.Game.Tournament;
+using osu.Web;
+
+
+// normal blazor stuff
+
+// --------
+
+public static partial class Program
+{
+#if DEBUG
+    private const string base_game_name = @"osu-development";
+#else
+        private const string base_game_name = @"osu";
+#endif
+
+    public static void Main(string[] args)
+    {
+
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Build().RunAsync().ConfigureAwait(false);
+
+
+        string gameName = base_game_name;
+        bool tournamentClient = false;
+
+        using (DesktopGameHost host = Host.GetSuitableDesktopHost(gameName, new HostOptions { BindIPC = false }))
+        {
+            if (tournamentClient)
+                host.Run(new TournamentGame());
+            else
+                host.Run(new OsuGameWeb(args));
+        }
+    }
+
+}
