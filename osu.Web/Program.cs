888f73d3b01db702013bb063cf2d19f73e1a021e
kone <70107937+konekowo@users.noreply.github.com>
2023-07-17 21:15:01 -0400

    stuff

--- osu.Web/Program.cs
+++ osu.Web/Program.cs
@@ -1,8 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using osu.Framework;
 using osu.Framework.Platform;
 using osu.Game.Tournament;
@@ -21,7 +23,7 @@ public static partial class Program
         private const string base_game_name = @"osu";
 #endif
 
-    public static void Main(string[] args)
+    public static async void Main(string[] args)
     {
 
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -30,8 +32,7 @@ public static partial class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        builder.Build().RunAsync().ConfigureAwait(false);
-
+        await builder.Build().RunAsync().ConfigureAwait(true);
 
         string gameName = base_game_name;
         bool tournamentClient = false;
